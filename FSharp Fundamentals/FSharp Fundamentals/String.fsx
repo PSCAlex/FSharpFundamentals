aa891f86dbd2dd971472c7e27bc7ff8d2840d21c
PSCAlex <alex@pscpayroll.com>
2017-01-04 09:21:15 +0000

    initial commit

---
+++ FSharp Fundamentals/FSharp Fundamentals/String.fsx
@@ -0,0 +1,16 @@
+﻿"It was the best of times,
+it was the worst of times"
+
+"""This "string" includes double quotes"""
+
+@"This ""string"" includes double quotes"
+
+//try without @
+@"Verbatim strings\n don't\t encode\n escape sequences"
+
+String.forall System.Char.IsDigit "03242asdf43"
+
+String.init 10 (fun i -> i * 10 |> string)
+
+
+ 
